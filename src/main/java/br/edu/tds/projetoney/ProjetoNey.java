a6402924c241d5e370928d6a84294b46f790893d
teio37 <lucas19.silva@alunos.ifsuldeminas.edu.br>
2026-02-09 17:11:08 -0300

    Primeiro codigo do projectNey

---
+++ src/main/java/br/edu/tds/projetoney/ProjetoNey.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.tds.projetoney;
+
+/**
+ *
+ * @author aluno
+ */
+public class ProjetoNey {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Neymar");
+    }
+}
